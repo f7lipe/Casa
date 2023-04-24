9cc93d1827cebe1b97b12901223bcfd9752020cd
= <f7lipe@me.com>
2023-04-23 22:51:57 -0300

    recreate home view

---
+++ app/(home)/index.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { Link } from 'expo-router';
+import { StyleSheet, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
+import { Text, View } from '../../components/Themed';
+import NoRooms from '../../components/NoRooms'
+import { useRoom } from '../../hooks/useRoom'
+import { RoomItem } from '../../components/RoomItem';
+
+interface item {
+  name: string
+}
+const Item = ({ name } : item) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{name}</Text>
+  </View>
+);
+
+export default function Page() {
+  const { rooms } = useRoom()
+  const [numColumns, setNumColumns] = useState(2);
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.main}>
+          {
+            !rooms.length && (
+              <NoRooms />
+            )
+          }
+          {
+            rooms.length > 0 && (
+              <FlatList
+              style={styles.flatList}
+                data={rooms}
+                key={2}
+                numColumns={2}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <RoomItem name={item.name} icon={item.icon} />}
+              />
+            )
+          }
+        </View>
+        <Link href="/room-creator" asChild>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Adicionar cômodo</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+    justifyContent: "space-between"
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: '100%',
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#ffd94e',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 24,
+    marginBottom: 24,
+    position: 'absolute',
+    bottom: 0
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#232323',
+  },
+  flatList: {
+    width: '100%',
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+})
