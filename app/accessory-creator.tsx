ececb0da5fd6b6736647a64f6aa40b6f8180e49d
= <f7lipe@me.com>
2023-04-25 12:30:21 -0300

    Create accessory-creator.tsx

---
+++ app/accessory-creator.tsx
@@ -0,0 +1,162 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native'
+import { Text, View } from '../components/Themed'
+import { useRoom } from '../hooks/useRoom'
+import { useLocalSearchParams, Link, useRouter } from "expo-router"
+import { BarCodeScanner, BarCodeScannedCallback, BarCodeEvent } from 'expo-barcode-scanner';
+import uuid from 'react-native-uuid'
+
+interface QRCodeData {
+    portNumber: number
+    name: string
+    type: Accessory
+}
+
+
+function AccessoryCreator() {
+    const { roomId } = useLocalSearchParams() as { roomId: string }
+    const { addAccessory } = useRoom()
+    const router = useRouter()
+
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null)
+    const [scanned, setScanned] = useState<boolean>(false)
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync()
+            setHasPermission(status === 'granted')
+        })()
+    }, [])
+
+    function createAccessoryFromQRCode(qrCodeData: QRCodeData, roomId: string): Accessory {
+        const { portNumber, name, type } = qrCodeData;
+      
+        switch (type) {
+          case 'light':
+            return {
+              id: uuid.v4(),
+              roomId,
+              port: portNumber,
+              name,
+              icon: 'bulb-off.png',
+              state: 'off',
+              type: 'light',
+            }
+          case 'fan':
+            return {
+              id: uuid.v4(),
+              roomId,
+              port: portNumber,
+              name,
+              icon: 'fan',
+              state: 'off',
+              type: 'fan',
+            }
+          case 'garage':
+            return {
+              id: uuid.v4(),
+              roomId,
+              port: portNumber,
+              name,
+              icon: 'garage',
+              state: 'off',
+              type: 'garage',
+            }
+          case 'outlet':
+            return {
+              id: uuid.v4(),
+              roomId,
+              port: portNumber,
+              name,
+              icon: 'outlet',
+              state: 'off',
+              type: 'outlet',
+            }
+          case 'rgb-led':
+            return {
+              id: uuid.v4(),
+              roomId,
+              port: portNumber,
+              name,
+              icon: 'led',
+              state: 'off',
+              type: 'led',
+            }
+          default:
+            throw new Error(`Invalid accessory type: ${type}`);
+        }
+      }
+      
+      const handleBarCodeScanned: BarCodeScannedCallback = (data: BarCodeEvent) => {
+        setScanned(true);
+        const qrCodeData = JSON.parse(data.data) as QRCodeData;
+        const newAccessory = createAccessoryFromQRCode(qrCodeData, roomId);
+        addAccessory(roomId, newAccessory);
+        router.back();
+      }
+      
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <BarCodeScanner 
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} 
+                style={StyleSheet.absoluteFillObject} />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        width: '100%',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    centerText: {
+        textAlign: 'left',
+    },
+    addButton: {
+        backgroundColor: '#000000',
+        borderRadius: 20,
+        padding: 10,
+        marginTop: 20,
+        height: 50,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        tintColor: '#fff',
+    },
+    picker: {
+        width: 150,
+        height: 150,
+        borderRadius: 50,
+        backgroundColor: '#c7c7c765',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    }
+})
+
+export default AccessoryCreator
