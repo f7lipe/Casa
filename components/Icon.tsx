beed3c5987c1272abcaaa8128495e069c807a5d0
= <f7lipe@me.com>
2023-04-27 09:03:32 -0300

    refactor icon component

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
-import * as Icons from '../assets/icons/'
+import { View, useColorScheme } from 'react-native';
+import * as Icons from '../assets/icons/';
+import { IconName } from '../@types/icon';
 
-interface Props {
-  name: string;
+interface IconProps {
+  name: IconName;
   size?: number;
   color?: string;
 }
 
-export default function Icon({ name = "Add", size = 32, color = "black" }: Props) {
-
+const Icon: React.FC<IconProps> = ({ name = 'Add', size = 32, color }: IconProps) => {
   const IconComponent = Icons[name];
 
   if (!IconComponent) {
     console.warn(`Icon "${name}" não encontrado`);
-    
     return null;
   }
 
+  const colorScheme = useColorScheme() || 'light';
+  const colorName = color || (colorScheme === 'dark' ? 'white' : 'black');
+
   return (
     <View>
-      <IconComponent width={size} height={size} fill={color} />
+      <IconComponent width={size} height={size} style={{ color: colorName }} />
     </View>
-  )
-}
+  );
+};
+
+export default Icon;
