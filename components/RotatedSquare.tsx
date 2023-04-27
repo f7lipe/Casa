e5424f326f946c5e58a2509d7a45f80ac2ee4326
= <f7lipe@me.com>
2023-04-27 09:06:17 -0300

    add support to dark mode

--- components/RotatedSquare.tsx
+++ components/RotatedSquare.tsx
@@ -1,31 +1,32 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, useColorScheme } from 'react-native'
+import React from 'react'
 
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export default function RotatedSquare({ children }: Props) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.children}>
-                {children}
-            </View>
-        </View>
-    )
+  const colorScheme = useColorScheme() || 'light'
+  const colorName = colorScheme === 'dark' ? 'white' : 'black'
+
+  return (
+    <View style={[styles.container, { borderColor: colorName }]}>
+      <View style={styles.children}>{children}</View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: 80,
-        height: 80,
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        transform: [{ rotate: '45deg' }],
-    },
-    children: {
-        transform: [{ rotate: '-45deg' }],
-    },
+  container: {
+    width: 80,
+    height: 80,
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: [{ rotate: '45deg' }],
+  },
+  children: {
+    transform: [{ rotate: '-45deg' }],
+  },
 })
