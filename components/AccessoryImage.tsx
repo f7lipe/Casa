8e7f2626cefe151611a09305d90398903b1a9686
= <f7lipe@me.com>
2023-05-01 11:30:24 -0300

    refactor AccessoryImage component

--- components/AccessoryImage.tsx
+++ components/AccessoryImage.tsx
@@ -15,16 +15,21 @@ export default function AccessoryImage({ accessoryName, size, isOn }: Props) {
     const IMAGES = {
         BULB_OFF: require("../assets/images/bulb-off.png"),
         BULB_ON: require("../assets/images/bulb-on.png"),
+        TV_OFF: require("../assets/images/tv-off.png"),
+        TV_ON: require("../assets/images/tv-on.png"),
     }
 
     useEffect(() => {
         const images = {
             bulb: isOn ? IMAGES.BULB_ON : IMAGES.BULB_OFF,
+            tv: isOn ? IMAGES.TV_ON : IMAGES.TV_OFF,
         }
         setAccessoryImage(images)
     }, [isOn])
 
     return (
-        <Image style={{ width: size, height: size }} source={accessoryImage[accessoryName]} />
+        <Image 
+            style={{ width: size, height: size }} 
+            source={accessoryImage[accessoryName]} />
     )
 }
