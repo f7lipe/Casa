ab8cb73795d9ffb3f1fd33c794b5a9c9efed2c3d
= <f7lipe@me.com>
2023-05-01 10:03:59 -0300

    add new icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,41 +1,135 @@
 import Ac from './ac.svg'
 import Add from './add.svg'
 import Bath from './bath.svg'
+import Bathtub from './bathtub.svg'
 import Bed from './bed.svg'
 import Bell from './bell.svg'
+import Books from './books.svg'
 import Bulb from './bulb.svg'
+import Bulbs from './bulbs.svg'
 import Cam from './cam.svg'
+import Cabinet from './cabinet.svg'
+import Chair from './chair.svg'
+import ChairLounge from './chair-lounge.svg'
 import Chandelier from './chandelier.svg'
-import Curtain from './curtain.svg'
-import Doorway from './doorway.svg'
-import GarageDoor from './garage-door.svg'
+import ChevronDown from './chevron-down.svg'
+import ChevronLeft from './chevron-left.svg'
+import ChevronRight from './chevron-right.svg'
+import ChevronUp from './chevron-up.svg'
+import Close from './close.svg'
+import Compass from './compass.svg'
 import Cooktop from './cooktop.svg'
+import Cup from './cup.svg'
+import Curtain from './curtain.svg'
+import Dot from './dot.svg'
+import Doorway from './doorway.svg'
+import Dryer from './dryer.svg'
+import Dumbbel from './dumbbell.svg'
+import ForkKinef from './fork-knife.svg'
+import FryingPan from './frying-pan.svg'
+import GameController from './game-controller.svg'
+import GarageDoor from './garage-door.svg'
+import Hammer from './hammer.svg'
 import Laundry from './laundry.svg'
-import Sofa from './sofa.svg'
+import Menu from './menu.svg'
+import Minus from './minus.svg'
+import Mic from './mic.svg'
+import Mug from './mug.svg'
 import NightShift from './nightshift.svg'
 import OfficeLamp from './office-lamp.svg'
 import Outlet from './outlet.svg'
+import PaintBrush from './paint-brush.svg'
+import PlayPause from './play-pause.svg'
+import PowerOff from './power-off.svg'
+import PowerOn from './power-on.svg'
+import Printer from './printer.svg'
+import QrCodeScan from './qr-code-scan.svg'
+import Radio from './radio.svg'
 import Refrigerator from './refrigerator.svg'
+import Room from './room.svg'
+import ScrewDriver from './screw-driver.svg'
+import Sliders from './sliders.svg'
+import Sofa from './sofa.svg'
+import Spigot from './spigot.svg'
+import SpeakerMuted from './speaker-muted.svg'
+import SpeakerSound from './speaker-sound.svg'
+import SpeakerSoundMin from './speaker-sound-min.svg'
+import SpeakerSoundMax from './speaker-sound-max.svg'
+import StarList from './star-list.svg'
+import SportsCourt from './sports-court.svg'
+import TeddyBear from './teddy-bear.svg'
+import TennisRacket from './tennis-racket.svg'
+import Tree from './tree.svg'
+import VideoProjector from './video-projector.svg'
+import WineGlass from './wine-glass.svg'
+import WaterWaves from './water-waves.svg'
 import Yard from './yard.svg'
 
 export {
     Ac,
     Add,
     Bath,
+    Bathtub,
     Bed,
     Bell,
+    Books,
     Bulb,
+    Bulbs,
     Cam,
+    Cabinet,
+    Chair,
+    ChairLounge,
     Chandelier,
-    Curtain,
-    Doorway,
-    GarageDoor,
+    ChevronDown,
+    ChevronLeft,
+    ChevronRight,
+    ChevronUp,
+    Close,
+    Compass,
     Cooktop,
+    Cup,
+    Curtain,
+    Dot,
+    Doorway,
+    Dryer,
+    Dumbbel,
+    ForkKinef,
+    FryingPan,
+    GameController,
+    GarageDoor,
+    Hammer,
     Laundry,
-    Sofa,
+    Menu,
+    Minus,
+    Mic,
+    Mug,
     NightShift,
     OfficeLamp,
     Outlet,
+    PaintBrush,
+    PlayPause,
+    PowerOff,
+    PowerOn,
+    Printer,
+    QrCodeScan,
+    Radio,
     Refrigerator,
+    Room,
+    ScrewDriver,
+    Sliders,
+    Sofa,
+    Spigot,
+    SpeakerMuted,
+    SpeakerSound,
+    SpeakerSoundMin,
+    SpeakerSoundMax,
+    StarList,
+    SportsCourt,
+    TeddyBear,
+    TennisRacket,
+    Tree,
+    VideoProjector,
+    WineGlass,
+    WaterWaves,
     Yard,
-}
+  }  
--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import { View, useColorScheme } from 'react-native';
-import * as Icons from '../assets/icons/';
-import { IconName } from '../@types/icon';
+import React from 'react'
+import { View, useColorScheme } from 'react-native'
+import * as Icons from '../assets/icons/'
+import { IconName } from '../@types/icon'
 
 interface IconProps {
-  name: IconName;
-  size?: number;
-  color?: string;
+  name: IconName
+  size?: number
+  color?: string
 }
 
 const Icon: React.FC<IconProps> = ({ name = 'Add', size = 32, color }: IconProps) => {
-  const IconComponent = Icons[name];
+  const IconComponent = Icons[name]
 
   if (!IconComponent) {
-    console.warn(`Icon "${name}" não encontrado`);
-    return null;
+    console.warn(`Icon "${name}" not found`)
+    return null
   }
 
-  const colorScheme = useColorScheme() || 'light';
-  const colorName = color || (colorScheme === 'dark' ? 'white' : 'black');
+  const colorScheme = useColorScheme() || 'light'
+  const colorName = color || (colorScheme === 'dark' ? 'white' : 'black')
 
   return (
     <View>
-      <IconComponent width={size} height={size} style={{ color: colorName }} />
+      <IconComponent 
+          width={size} 
+          height={size} 
+          style={{ color: colorName }} />
     </View>
-  );
-};
+  )
+}
 
-export default Icon;
+export default Icon
